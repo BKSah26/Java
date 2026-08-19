565a2150eb6b9d2d8262ff14d2a3aed4fe34af6e
Bipin Kumar Sah <sahbipinkr2006@gmail.com>
2026-08-19 21:22:32 +0530

    Modification in Complex Number code

--- src/Object_Oriented_Programming/ComplexNumbers.java
+++ src/Object_Oriented_Programming/ComplexNumbers.java
@@ -11,19 +11,30 @@ public class ComplexNumbers {
         }
 
         void print(){
-            if (y<0){
-                System.out.println(x+" - "+(-y)+"i");
+            if (x<0){
+                System.out.print("-"+(-x));
             }
             else{
-                System.out.println(x+" + "+y+"i");
+                System.out.print(x);
             }
+            if (y<0){
+                System.out.print(" - "+(-y)+"i");
+            }
+            else{
+                System.out.print(" + "+y+"i");
+            }
+            System.out.println();
         }
     }
 
     public static void main(String[] args) {
         ComplexNumber c1 = new ComplexNumber(1, -2);
         ComplexNumber c2 = new ComplexNumber(2, 5);
+        ComplexNumber c3 = new ComplexNumber(-6, 7);
+        ComplexNumber c4 = new ComplexNumber(-5, -5);
         c1.print();
         c2.print();
+        c3.print();
+        c4.print();
     }
 }
